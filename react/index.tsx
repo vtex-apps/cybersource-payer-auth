7d3234154972c3a10772433307a785b88c1504d6
polishq <arthur.bond@vtex.com.br>
2022-09-07 11:13:46 -0400

    Convert to typescript, linting

---
+++ react/index.tsx
@@ -0,0 +1,82 @@
+/* eslint-disable no-console */
+import React, { Component } from 'react'
+
+interface CyberSourceAuthenticationProps {
+  appPayload: string
+}
+
+declare const $: any
+
+class CybersourcePayerAuth extends Component<CyberSourceAuthenticationProps> {
+  public formRef = React.createRef<HTMLFormElement>()
+
+  public state = {
+    submitted: false,
+  }
+
+  public componentDidMount() {
+    console.log('componentDidMount =>', JSON.stringify(this.props.appPayload))
+  }
+
+  public componentDidUpdate() {
+    if (this.state.submitted) {
+      return
+    }
+
+    window.addEventListener(
+      'message',
+      event => {
+        if (event.origin === 'https://centinelapistag.cardinalcommerce.com') {
+          console.log(event.data)
+        }
+      },
+      false
+    )
+
+    if (this.formRef.current) {
+      this.formRef.current.submit()
+      this.setState({ submitted: true })
+    }
+  }
+
+  public respondTransaction(status: string) {
+    $(window).trigger('transactionValidation.vtex', [status])
+  }
+
+  public render() {
+    const { deviceDataCollectionUrl, accessToken } = JSON.parse(
+      this.props.appPayload
+    )
+
+    console.log('rendering...')
+
+    return (
+      <>
+        <iframe
+          id="cardinal_collection_iframe"
+          title="Cardinal Collection Iframe"
+          name="collectionIframe"
+          height="10"
+          width="10"
+          style={{ display: 'none' }}
+        />
+        <form
+          ref={this.formRef}
+          id="cardinal_collection_form"
+          method="POST"
+          target="collectionIframe"
+          action={deviceDataCollectionUrl}
+        >
+          <input
+            id="cardinal_collection_form_input"
+            type="hidden"
+            name="JWT"
+            value={accessToken}
+          />
+        </form>
+      </>
+    )
+  }
+}
+
+export default CybersourcePayerAuth
